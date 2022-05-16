b29355d392ae4ef12ae6600e165b3e909689f017
Юрий Иванчев <yura97108@gmail.com>
2022-05-16 17:41:40 +0300

    azure yml

--- env.cmd
+++ env.cmd
@@ -1,3 +1,3 @@
 SETLOCAL
 SET TestUrl=https://ivanchev-webcalculator.azurewebsites.net
-dotnet test
+dotnet test --filter FullyQualifiedName=WebCalculator.Tests.CalculatorControllerFunctionalTests --logger "trx;logfilename=test-results.trx" --results-directory TestResults/
